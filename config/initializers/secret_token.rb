e1a855cc3681b484edd3d7fb362fa03c6b46bd23
cristhian <alejandro.olmos@live.com>
2012-09-25 11:50:32 -0500

    Iniciando aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FemsaApp::Application.config.secret_token = 'd0f2eee1acb63e7d459bf693bc99401f52f6aa11a681a49558d94a4d6f41a6d0c9bb32a0a4ef4d0ceeb157507206cafbbc1e8fe236297977b457e57956b6ecb5'
